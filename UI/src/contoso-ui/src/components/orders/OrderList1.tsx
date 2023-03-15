fc93d0f15c7724d4276c58d63b18ea4a2728cef7
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-15 14:12:42 +0000

    Another way to use const

--- UI/src/contoso-ui/src/components/orders/OrderList1.tsx
+++ UI/src/contoso-ui/src/components/orders/OrderList1.tsx
@@ -2,11 +2,28 @@ import Button from "@mui/material/Button";
 import React from "react";
 import { Order } from "./Order";
 
+/*
 export const OrderList1: React.FC<{orders : Order[]}> = ({orders}) => {
   return(
     <div>
     <ul>
       {orders.map(order => {
+        return <li key={order.id}>Order :: {order.id}</li>
+      })}
+    </ul>
+  </div>
+  )
+}
+*/
+
+type MyProps = {
+  orders : Order[]
+}
+export const OrderList1 = (props: MyProps) => {
+  return(
+    <div>
+    <ul>
+      {props.orders.map(order => {
         return <li key={order.id}>Order :: {order.id}</li>
       })}
     </ul>
