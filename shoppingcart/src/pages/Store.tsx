bb3cfbc4736c934fcc7ab6ee587290007eff48a8
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-15 17:26:19 +0000

    showing name and price

--- shoppingcart/src/pages/Store.tsx
+++ shoppingcart/src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import { Col, Row } from "react-bootstrap"
+import { StoreItem } from "../components/StoreItem"
+import storedItems from "../data/items.json"
+
 export function Store(){
-  return <h1>Store</h1>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storedItems.map(item => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
