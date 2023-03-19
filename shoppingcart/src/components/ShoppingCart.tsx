4e759d6fe1758b764c7cccae80747f98484bf25d
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-19 00:36:34 +0000

    Added behaviours to Cart

--- shoppingcart/src/components/ShoppingCart.tsx
+++ shoppingcart/src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
 import { CartItem } from "./CartItem";
+import storeItems from "../data/items.json"
 
 type ShoppingCartProps = {
   isOpen: boolean
@@ -21,6 +23,13 @@ export function ShoppingCart({isOpen} : ShoppingCartProps){
           <CartItem key={item.id} {...item} />
         ))}
       </Stack>
+      <div className="ms-auto fw-bold fs-5">
+        Total {formatCurrency(cartItems.reduce((total, cartItem) => {
+            const item = storeItems.find(i => i.id === cartItem.id)
+            return total + (item?.price || 0) * cartItem.quantity
+          }, 0)
+        )}
+      </div>
     </Offcanvas.Body>
   </Offcanvas>)
 }
