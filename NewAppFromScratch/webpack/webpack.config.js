f499d80a716b3431c8de58f83bdcd3ebfbbf7f7a
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-22 10:54:51 +0000

    Fixed webpack config

--- NewAppFromScratch/webpack/webpack.config.js
+++ NewAppFromScratch/webpack/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(?:svg|ico|gif|png|jpg|jpeg)$/i,
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
       },
       {
