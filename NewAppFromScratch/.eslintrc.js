4e8dee1a70b39c87be82af6e14176ff5922fb4fa
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-22 13:44:49 +0000

    Update .eslintrc.js

--- NewAppFromScratch/.eslintrc.js
+++ NewAppFromScratch/.eslintrc.js
@@ -12,7 +12,11 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended'
   ],
   rules: {
     'no-unused-vars': 'off',
