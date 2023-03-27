e208482de68cc7815b631baff29dae713cddd706
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-27 13:25:01 +0100

    Added css to menu

--- learning-query/src/App.tsx
+++ learning-query/src/App.tsx
@@ -1,4 +1,4 @@
-
+import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { RQSuperheroes } from './pages/RQSuperheroes'
