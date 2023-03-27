ce784f2a68b6104a76c70b3486342a9e4d346d88
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-27 13:47:17 +0100

    Getting data from Fake API

--- learning-query/src/pages/Superheroes.tsx
+++ learning-query/src/pages/Superheroes.tsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+type Hero = {
+  id: number,
+  name: string,
+  alterEgo: string
+}
+
 export const Superheroes = () => {
+
+  const [isLoading, setLoading] = useState(true)
+  const [data, setData] = useState<Hero[]>([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/superheroes').then((res) => {
+      setData(res.data)
+      setLoading(false)
+    })
+  }, [])
+
+  if (isLoading){
+    return <h2>Loading...</h2>
+  }
+
   return (
-    <div>
-      Super heroes Page
-    </div>
+    <>
+      <h2>Super Heroes</h2>
+      {data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>
+      })}
+    </>
   )
 }
