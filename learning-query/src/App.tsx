3518410eb6adede13efca6e8d01087ef80212c8d
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-27 13:57:32 +0100

    refactored menu

--- learning-query/src/App.tsx
+++ learning-query/src/App.tsx
@@ -1,31 +1,38 @@
 import './App.css'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { RQSuperheroes } from './pages/RQSuperheroes'
 import { Superheroes } from './pages/Superheroes'
 
-function App() {
+const Menu = () => {
   return (
     <BrowserRouter>
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/super-heroes'>Traditional Super Heroes</Link>
-        </li>
-        <li>
-          <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/super-heroes'>Traditional Super Heroes</Link>
+          </li>
+          <li>
+            <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
           <Route path="/rq-super-heroes" element={<RQSuperheroes />}></Route>
           <Route path="/super-heroes" element={<Superheroes />}></Route>
           <Route path="/" element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
+    )
+}
+
+function App() {
+  return (
+    <Menu />
   )
 }
 
