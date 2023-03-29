101c63a6170cd0c79910aab88c1e409e2202c2ec
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-29 15:21:55 +0100

    moved button location

--- shoppingcart/src/App.tsx
+++ shoppingcart/src/App.tsx
@@ -5,34 +5,15 @@ import { Store } from "./pages/Store"
 import { About } from "./pages/About"
 import { Navbar} from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
-import { useTranslation, Trans } from 'react-i18next';
-
-interface Language {
-  code: string,
-  nativeName: string,
-  flag: string
-}
-
-const languages: Language[] = [
-  { code: 'pt', nativeName: 'Portuguese', flag: 'src/assets/pt_flag.svg' },
-  { code: 'en', nativeName: 'English', flag: 'src/assets/us_flag.svg' }
-]
+import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { t, i18n } = useTranslation();
+  useTranslation();
 
   return (
     <ShoppingCartProvider>
       <Navbar />
-      {languages.map(language => (
-        <span key={language.code} onClick={() => i18n.changeLanguage(language.code)}>
-          <img src={language.flag} style={{ 
-          width: "50px", 
-          height: "25px", 
-          objectFit: "cover" 
-        }}/>
-        </span>
-      ))}
+      
       <Container className="mb-4">
         <Routes>
           <Route path="/" element={<Home />} />
