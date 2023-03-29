4f2f990393dcd73e182b9c2437d0a56919e6ef54
Caio Calisto <caiocesarcalisto@gmail.com>
2023-03-29 15:44:14 +0100

    separated files for languages

--- shoppingcart/src/i18n.ts
+++ shoppingcart/src/i18n.ts
@@ -1,6 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import translationEN from '../public/locales/en/translation.json'
+import translationPT from '../public/locales/pt/translation.json'
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  pt: {
+    translation: translationPT
+  }
+};
 
 i18n
   // detect user language
@@ -11,54 +22,11 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources,
     debug: true,
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    },
-    resources: {
-      en: {
-        translation: {
-          menu:{
-            home: 'HOME',
-            store: 'STORE',
-            about: 'ABOUT'
-          },
-          about:{
-            title: 'More about us'
-          },
-          home:{
-            title: 'This is the home page'
-          },
-          item:{
-            addToCart: 'Add to cart',
-            remove: 'Remove',
-            inCart: 'in Cart',
-            cartTitle: 'Cart'
-          }
-        }
-      },
-      pt: {
-        translation: {
-          menu:{
-            home: 'INICIO',
-            store: 'LOJA',
-            about: 'SOBRE NÓS'
-          },
-          about:{
-            title: 'Mais sobre nós'
-          },
-          home:{
-            title: 'Esta é a pagina inicial'
-          },
-          item:{
-            addToCart: 'Adicionar ao carrinho',
-            remove: 'Remover',
-            inCart: 'no Carrinho',
-            cartTitle: 'Carrinho'
-          }
-        }
-      }
     }
   });
 
