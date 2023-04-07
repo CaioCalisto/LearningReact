826074172c796c948bcc9210906e19af13dae9b8
Caio Calisto <caiocesarcalisto@gmail.com>
2023-04-07 23:47:13 +0100

    more info in decorator

--- using-decorators/perfDecorator2.ts
+++ using-decorators/perfDecorator2.ts
@@ -1,13 +1,23 @@
 export function first() {
   console.log("first(): factory evaluated");
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    console.log("first(): called");
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    console.log('first(): called -- target: ' + JSON.stringify(target) + 
+      ', propertyKey: ' + propertyKey + 
+      ',descriptor: ' + JSON.stringify(descriptor));
   };
 }
- 
+
 export function second() {
   console.log("second(): factory evaluated");
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
     console.log("second(): called");
   };
 }
