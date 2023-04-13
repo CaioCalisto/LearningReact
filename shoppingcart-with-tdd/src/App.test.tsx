5f625f46d0849d792f7db63b24ce2b2c9c64d214
Caio Calisto <caiocesarcalisto@gmail.com>
2023-04-13 15:09:08 +0100

    Test navigation to other page

--- shoppingcart-with-tdd/src/App.test.tsx
+++ shoppingcart-with-tdd/src/App.test.tsx
@@ -1,23 +1,43 @@
-import { getByTestId, render, screen } from "@testing-library/react"
+import { render, fireEvent, screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom"
 import { App } from "./App"
+import { act } from "react-dom/test-utils";
 
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({
     t: (key: string) => {
-      return 'translation_not_found'
+      if (key === 'menu.home'){
+        return 'HOME'
+      }
+      if (key === 'menu.store'){
+        return 'STORE'
+      }
+      if (key === 'menu.about'){
+        return 'ABOUT'
+      }
     }
   }),
 }))
 
+
+
 describe('Main App', () => {
   test('Should show the nav menu', () => {
     const { container } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+        <App />, {wrapper: BrowserRouter}
     )
 
     expect(container.getElementsByClassName('navbar-nav').length).toBe(1)
+  })
+
+  test('Should contain a clickable link to Home page', async () => {
+    
+    const { getByText} = render(
+      <App />, {wrapper: BrowserRouter}
+    )
+
+    await fireEvent.click(getByText('HOME'))
+    
+    expect(screen.getByText(/HELLO WORLD/i)).toBeInTheDocument()
   })
 })
--- shoppingcart-with-tdd/src/App.tsx
+++ shoppingcart-with-tdd/src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import { Container } from 'react-bootstrap'
 import { Menu } from './components/Menu'
+import { Home } from './pages/Home'
 
 export const App = () => {
   return (
-    <Menu />
+    <>
+      <Menu />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
+    </>
   )
 }
