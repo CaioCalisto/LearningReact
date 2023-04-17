866431c041ef0f8d5ad397f42ab8f17cfe14f11a
Caio Calisto <caiocesarcalisto@gmail.com>
2023-04-17 14:01:24 +0100

    added Router

--- note-taking/src/App.tsx
+++ note-taking/src/App.tsx
@@ -1,8 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { NewNote } from "./components/NewNote"
 
 function App() {
   
   return (
-    <><h1>Hello</h1></>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>HOME</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   )
 }
 
---
+++ note-taking/src/components/NewNote.tsx
@@ -0,0 +1,7 @@
+export function NewNote(){
+  return (
+    <>
+      <h1 className="mb-4">New note</h1>
+    </>
+  )
+}
