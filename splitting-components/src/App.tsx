a37632a659311bac3009ffd253f6b3ea80c5d968
Caio Calisto <caiocesarcalisto@gmail.com>
2023-04-17 10:37:29 +0100

    Added menu and pages to use Lazy Loading

--- splitting-components/src/App.tsx
+++ splitting-components/src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import { Home } from './components/Home'
+import { Store } from './components/Store'
 
 function App() {
   
   return (
     <>
+    <NavWrapper />
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+        </Route>
+      </Routes>
+      <br /> 
       <button onClick={() => {
         // Here I only download this component when user click on the button
         import('./components/Something').then(module => {
@@ -12,6 +23,17 @@ function App() {
       }}>
         Click here
       </button>
+    </>
+  )
+}
+
+function NavWrapper(){
+  return (
+    <>
+      <nav>
+        <Link to="/">HOME</Link>
+        <Link to="/store">STORE</Link>
+      </nav>
     </>
   )
 }
---
+++ splitting-components/src/components/Store.tsx
@@ -0,0 +1,7 @@
+export const Store = () => {
+  return (
+    <>
+      <h1>STORE</h1>
+    </>
+  )
+}
