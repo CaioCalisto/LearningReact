d4105022aefb655d17d16a34cdfe6478b0c64389
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-11 15:42:34 +0100

    MUI: trying to rerender and test

--- MUI/mui-testing/src/components/MyForm.test.tsx
+++ MUI/mui-testing/src/components/MyForm.test.tsx
@@ -6,13 +6,14 @@ import '@testing-library/jest-dom/extend-expect'
 import MyForm from './MyForm'
 
 describe('Testing My Form that uses MUI', () => {
-  it('I want to just change a MUI Text box', async () => {
-    render(<MyForm />)
+  it('I want to just change a MUI Text box', () => {
+    const { getByText } = render(<MyForm />)
     var myValue = 'whatever I want as a value'
     
     var textBox = screen.getByTestId('myTextField').querySelector('input')
     fireEvent.change(textBox, { target: { value: myValue }})
 
     expect(textBox.value).toBe(myValue)
+    // expect(getByText(myValue)).not.toBeNull()
   })
 })
