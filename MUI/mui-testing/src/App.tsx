9694a1a366b43a91eb5f1813247a8a295122d62e
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-11 14:47:36 +0100

    clean

--- MUI/mui-testing/src/App.tsx
+++ MUI/mui-testing/src/App.tsx
@@ -1,20 +1,7 @@
 import React from 'react'
-import './styles.css'
-import logo from './assets/logo-react.svg'
-import { ClickCounter } from './ClickCounter'
 
 export const App = () => {
-  const name = "Caioo"
   return (
-    <div>
-      <h1>React from scratch.</h1>
-      <br />
-      <h1>Environment {process.env.NODE_ENV}</h1>
-      <h1>Name: {process.env.name}</h1>
-      <br />
-      <img src={logo} alt="React Logo" width="300" height="200" />
-      <br />
-      <ClickCounter />
-    </div>
+    <h1>hello</h1>
   )
 }
