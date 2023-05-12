8df0f4c99d7a1a154072a3e9a8f14c34d48290d5
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-12 13:44:12 +0100

    MUI: testing

--- MUI/mui-testing/src/components/MyFormParent.test.tsx
+++ MUI/mui-testing/src/components/MyFormParent.test.tsx
@@ -4,11 +4,16 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import MyFormParent from './MyFormParent'
 
-jest.mock("./MyForm", () => {
-  return function FakeComponent(props) {
+jest.mock('./MyForm', () => {
+  return function FakeComponent({ myCallback }) {
     return (
       <>
-        <button onClick={() => props.myCallback('My Custom Value sent from callback')} data-testid='myButton'>Click</button>
+        <button
+          onClick={() => myCallback('My Custom Value sent from callback')}
+          data-testid="myButton"
+        >
+          Click
+        </button>
       </>
     )
   }
@@ -17,7 +22,7 @@ jest.mock("./MyForm", () => {
 describe('Testing Parent component', () => {
   it('I want to test this parent isolated when children does a callback', () => {
     const { getByText } = render(<MyFormParent />)
-    
+
     var button = screen.getByTestId('myButton')
     fireEvent.click(button)
 
