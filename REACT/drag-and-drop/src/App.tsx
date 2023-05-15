f35ef33148636960e2c45bea25c9fc549b8ab954
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-15 11:03:05 +0100

    React: drag and drop

---
+++ REACT/drag-and-drop/src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+  function allowDrop(ev){
+    ev.preventDefault()
+  }
+
+  function drag(ev) {
+    ev.dataTransfer.setData("item", ev.target.id)
+  }
+
+  function drop(ev) {
+    ev.preventDefault()
+    const el = ev.dataTransfer.getData("item")
+
+    ev.target.appendChild(document.getElementById(el))
+  }
+  
+  return (
+    <>
+      <div className="box" onDrop={() => drop(event)} onDragOver={() => allowDrop(event)}>
+        <div id="item" draggable="true" onDragStart={() => drag(event)}>
+        </div>
+      </div>
+      <div className="box" onDrop={() => drop(event)} onDragOver={() => allowDrop(event)}>
+        <div id="item" draggable="true" onDragStart={() => drag(event)}>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
