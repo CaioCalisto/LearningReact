b80d8a03afeb39b9a64a843cf2a199d052ce69ba
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-16 12:01:16 +0100

    Login is working

--- REACT/login-2/src/components/Dashboard.tsx
+++ REACT/login-2/src/components/Dashboard.tsx
@@ -4,14 +4,14 @@ import { Navigate } from "react-router-dom";
 function Dashboard() {
   var authenticated = localStorage.getItem('authenticated')
 
-  if (authenticated === "false") {
-    return <Navigate replace to="/login" />;
-  } else {
+  if (authenticated === "true") {
     return (
       <div>
         <p>Welcome to your Dashboard</p>
       </div>
     );
+  } else {
+    return <Navigate replace to="/login" />;
   }
 }
 
