c7b0e34290114cd7b3893001eb878c3b82a3c3c0
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-19 15:16:05 +0100

    REACT: useRef

--- REACT/using-ref/src/App.tsx
+++ REACT/using-ref/src/App.tsx
@@ -1,18 +1,34 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
   const [name, setName] = useState("")
   const [renderCount, setRenderCount] = useState(0)
 
+  const renderCountRef = useRef(0)
+
   useEffect(() => {
-    setRenderCount(prevRenderCount => prevRenderCount + 1)
+    // With this line I can see inumerous number
+    // setRenderCount(prevRenderCount => prevRenderCount + 1)
+    
+    // with this line I can this counter being increments fews time
+    renderCountRef.current = renderCountRef.current + 1
   })
+
+  const prevName = useRef('')
+
+  // I want to render only when name changed
+  useEffect(() => {
+    prevName.current = name
+  }, [name])
 
   return (
     <>
       <input placeholder='Type your name' value={name} onChange={e => setName(e.target.value)} />
       <h1>My name is {name}</h1>
-      <div>I rendered {renderCount} times</div>
+      <div>I rendered {renderCount} times. renderCount</div>
+      <div>I rendered {renderCountRef.current} times. renderCountRef.current</div>
+      <br /><br />
+      <div>Previous name: {prevName.current}</div>
     </>
   )
 }
