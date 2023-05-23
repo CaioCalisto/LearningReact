99c7eab9f792414098f3738aadfe4cac8fa57cf5
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-23 21:04:06 +0100

    query with error: routes

--- REACT/query-with-error-boundary/src/App.tsx
+++ REACT/query-with-error-boundary/src/App.tsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Friends from './pages/Friends'
 
 function App() {
   
   return (
     <>
-      <h1>Home</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/friends">Friends</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/friends' element={<Friends />}/>
+        <Route path='*' element={<h1>Page not found!</h1>} />
+      </Routes>
     </>
   )
 }
