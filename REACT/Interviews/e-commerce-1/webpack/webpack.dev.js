0897e34ca09d7721df9a20a756774f5e3f07e108
Caio Calisto <caio.calisto@ipcortex.co.uk>
2023-05-31 22:44:11 +0100

    env

--- REACT/Interviews/e-commerce-1/webpack/webpack.dev.js
+++ REACT/Interviews/e-commerce-1/webpack/webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     new ReactRefreshWebpackPlugin(),
     new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('Caio'),
+      'process.env.owner': JSON.stringify('Caio Cesar Calisto'),
     }),
   ],
 }
--- REACT/Interviews/e-commerce-1/webpack/webpack.prod.js
+++ REACT/Interviews/e-commerce-1/webpack/webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('Codevolution'),
+      'process.env.owner': JSON.stringify('Caio Cesar Calisto'),
     }),
     new BundleAnalyzerPlugin(),
   ]
