05edf637777401509b784a6740617224bcd67686
Caio Calisto <caio.calisto@ipcortex.co.uk>
2023-06-07 23:22:09 +0100

    challenge: routes

--- REACT/Interviews/e-commerce/src/main.tsx
+++ REACT/Interviews/e-commerce/src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Store from './pages/Store'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Store />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
