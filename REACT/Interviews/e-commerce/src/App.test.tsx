a0bb4ff68d31c7f7c1007702f674b4ce040261c3
Caio Calisto <caio.calisto@ipcortex.co.uk>
2023-06-08 13:14:10 +0100

    cleaning

--- REACT/Interviews/e-commerce/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-describe('App test', () => {
-  it ('First test', () => {
-    render(<App />)
-    expect(screen.getByText(/Hello/)).toBeInTheDocument()
-  })
-})
