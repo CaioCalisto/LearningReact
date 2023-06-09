0bfa6cbdae5a777bcbf3ef6960451b26f2277732
Caio Calisto <caio.calisto@ipcortex.co.uk>
2023-06-09 23:05:14 +0100

    MUI layout - menu

--- MUI-THEME/custom-layout/src/components/Header.tsx
+++ MUI-THEME/custom-layout/src/components/Header.tsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
-import { IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  SwipeableDrawer,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import MailIcon from "@mui/icons-material/Mail";
+
 function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleDrawer = (open: boolean) => {
+    setOpenMenu(open);
+  };
+
+  const sideMenu = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={() => toggleDrawer(false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -12,13 +66,22 @@ function Header() {
           color="inherit"
           aria-label="menu"
           sx={{ mr: 2 }}
+          onClick={() => toggleDrawer(true)}
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Acme Co
         </Typography>
       </Toolbar>
+      <SwipeableDrawer
+        anchor="left"
+        open={openMenu}
+        onClose={() => toggleDrawer(false)}
+        onOpen={() => toggleDrawer(true)}
+      >
+        {sideMenu()}
+      </SwipeableDrawer>
     </AppBar>
   );
 }
