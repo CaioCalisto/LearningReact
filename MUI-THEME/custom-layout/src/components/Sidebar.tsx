7120b9215cfa4bb86cdd9c32f367ad9eda46521e
Caio Calisto <caio.calisto@ipcortex.co.uk>
2023-06-10 18:10:54 +0100

    MUI layout

--- MUI-THEME/custom-layout/src/components/Sidebar.tsx
+++ MUI-THEME/custom-layout/src/components/Sidebar.tsx
@@ -1,11 +1,41 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import MailIcon from "@mui/icons-material/Mail";
 import './Sidebar.css'
 
 function Sidebar() {
+  const sideMenu = () => (
+    <>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <MailIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+
   return (
     <>
-      <h3>SidebarSidebarSidebarSidebar</h3>
+      {sideMenu()}
     </>
   );
 }
