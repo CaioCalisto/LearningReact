bcaaa58cd9a6564abfaec12888edf1b2ad6c5672
Caio Calisto <caiocesarcalisto@gmail.com>
2023-08-05 22:52:25 +0100

    setup first test

--- TESTING/learning-mocks/src/__tests__/Pag1.test.tsx
+++ TESTING/learning-mocks/src/__tests__/Pag1.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import App from "../App";
+import React from "react";
+import Page1 from '../pages/Page1'
 
-describe("App", () => {
-  it("should work as expected", () => {
-    // render(<App />);
-    expect(1 + 1).toBe(2);
+describe("Hook 1 - mock for all tests", () => {
+  it("Test 1", () => {
+    render(<Page1 />)
+    expect(screen.getByText(/Page1/)).toBeVisible()
   });
 });
--- TESTING/learning-mocks/src/pages/Page1.tsx
+++ TESTING/learning-mocks/src/pages/Page1.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import useUser from '../hooks/useUser'
 
 function Page1() {
+  //const { username, email, someAction, anotherAction, asyncAction } = useUser()
+
   return (
     <div>Page1</div>
   )
