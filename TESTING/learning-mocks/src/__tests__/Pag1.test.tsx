acad8b6ffe628f1097d66a3d105af59148b9e07c
Caio Calisto <caiocesarcalisto@gmail.com>
2023-08-06 21:25:52 +0100

    Mock hook with const and functions

--- TESTING/learning-mocks/src/__tests__/Pag1.test.tsx
+++ TESTING/learning-mocks/src/__tests__/Pag1.test.tsx
@@ -1,26 +1,56 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
-import Page1 from '../pages/Page1'
+import Page1 from "../pages/Page1";
 import { useUser } from "../hooks/useUser";
 
-jest.mock('../hooks/useUser')
+jest.mock("../hooks/useUser");
 
 describe("Hook 1 - mock for all tests", () => {
   it("Mock constant 1", () => {
     useUser.mockReturnValue({
-      username: 'calicc'
-    })
+      username: "calicc",
+    });
 
-    render(<Page1 />)
-    expect(screen.getByText(/calicc/)).toBeVisible()
+    render(<Page1 />);
+    expect(screen.getByText(/calicc/)).toBeVisible();
   });
 
   it("Mock constant 2", () => {
     useUser.mockReturnValue({
-      email: 'caio.calisto@gmail.com'
-    })
+      email: "caio.calisto@gmail.com",
+    });
 
-    render(<Page1 />)
-    expect(screen.getByText(/caio.calisto@gmail.com/)).toBeVisible()
-  })
+    render(<Page1 />);
+    expect(screen.getByText(/caio.calisto@gmail.com/)).toBeVisible();
+  });
+
+  it("Mock function call", () => {
+    const myFunction = jest.fn();
+    useUser.mockReturnValue({
+      someAction: myFunction,
+    });
+    render(<Page1 />);
+
+    fireEvent.click(screen.getByTestId("button1"));
+
+    expect(myFunction).toHaveBeenCalledTimes(1);
+  });
+
+  it("Mock function with parameters", () => {
+    const myFunction = jest.fn();
+    const now = "2020-05-10T10:13:08.000Z";
+    const usernameId = "calicc";
+    jest.useFakeTimers().setSystemTime(new Date(now));
+    useUser.mockReturnValue({
+      username: usernameId,
+      anotherAction: myFunction,
+    });
+    render(<Page1 />);
+
+    fireEvent.click(screen.getByTestId("button2"));
+    expect(myFunction).toHaveBeenCalledTimes(1);
+    expect(myFunction).toHaveBeenCalledWith(usernameId, now);
+  });
+
+  it("Mock async Action", () => {});
 });
--- TESTING/learning-mocks/src/pages/Page1.tsx
+++ TESTING/learning-mocks/src/pages/Page1.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { useUser } from '../hooks/useUser'
+import React from "react";
+import { useUser } from "../hooks/useUser";
 
 function Page1() {
-  const { username, email, someAction, anotherAction, asyncAction } = useUser()
+  const { username, email, someAction, anotherAction, asyncAction } = useUser();
 
   return (
     <>
       <p>{username}</p>
       <p>{email}</p>
-      <button data-testid='button1' onClick={() => someAction}>Button 1</button>
+      <button data-testid="button1" onClick={someAction}>
+        Button 1
+      </button>
+      <button
+        data-testid="button2"
+        onClick={() => anotherAction(username, new Date().toISOString())}
+      >
+        Button 2
+      </button>
     </>
-  )
+  );
 }
 
-export default Page1
+export default Page1;
