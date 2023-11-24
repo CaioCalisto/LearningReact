8ce9cb00c8ded565360d9391c89528868b4290ed
Caio Calisto <caiocesarcalisto@gmail.com>
2023-11-24 11:27:58 +0000

    added reactflow style, minimap and controls

--- NEXTJS/first-app/src/app/flow/page.tsx
+++ NEXTJS/first-app/src/app/flow/page.tsx
@@ -1,18 +1,18 @@
 'use client'
-import {
+import ReactFlow, {
   Background,
   BackgroundVariant,
   Controls,
-  MiniMap,
-  ReactFlow,
-} from 'reactflow'
+  MiniMap
+} from 'reactflow';
+import 'reactflow/dist/style.css';
 
 export default function FlowPage() {
   return (
     <>
       <ReactFlow>
-        <Controls />
-        <MiniMap />
+        <Controls position={'bottom-left'} />
+        <MiniMap pannable position={'bottom-right'} />
         <Background variant={BackgroundVariant.Lines} gap={12} size={1} />
       </ReactFlow>
     </>
