ab3bf1160810972e447d37cd9f690a552b6c5a10
Caio Calisto <caiocesarcalisto@gmail.com>
2023-11-24 10:34:14 +0000

    new nextJs app

---
+++ TESTING/learning-mocks/src/__tests__/Page1-different-way.test.tsx
@@ -0,0 +1,25 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import Page1 from '../pages/Page1';
+import {useUser} from '../hooks/useUser';
+
+const useUserMock = jest.spyOn() // how can I mock ?
+
+describe('Hook 1 - mock for all tests', () => {
+  it('Mock constant 1', () => {
+    const response = {
+      anotherAction(id: string, timestamp: string): boolean {
+        return false;
+      }, asyncAction(): Promise<boolean> {
+        return Promise.resolve(false);
+      }, email: '', someAction(): void {
+      },
+      username: 'calicc',
+    };
+
+    useUserMock.mockReturnValue(response);
+
+    render(<Page1 />);
+    expect(screen.getByText(/calicc/)).toBeVisible();
+  });
+});
