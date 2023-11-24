2b468ac90919ade0a08e7b7ddbf6afa903cbaef3
Caio Calisto <caiocesarcalisto@gmail.com>
2023-11-24 11:18:02 +0000

    added reactflow

--- NEXTJS/first-app/src/app/flow/page.tsx
+++ NEXTJS/first-app/src/app/flow/page.tsx
@@ -1,7 +1,20 @@
+'use client'
+import {
+  Background,
+  BackgroundVariant,
+  Controls,
+  MiniMap,
+  ReactFlow,
+} from 'reactflow'
+
 export default function FlowPage() {
   return (
     <>
-      <p>FLOW</p>
+      <ReactFlow>
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Lines} gap={12} size={1} />
+      </ReactFlow>
     </>
   )
 }
