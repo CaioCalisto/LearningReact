e28bacb102109cf08cba1e6945312ffa53e3f13f
Caio Calisto <caiocesarcalisto@gmail.com>
2023-11-25 21:22:06 +0000

    improve menu layout

--- NEXTJS/first-app/src/app/flow/menus/add/add-node.tsx
+++ NEXTJS/first-app/src/app/flow/menus/add/add-node.tsx
@@ -1,20 +1,18 @@
 import styles from './add-node.module.scss'
-import { MdAdd } from "react-icons/md";
-import { MdDialpad } from "react-icons/md";
+import { MdAdd } from 'react-icons/md'
+import { MdDialpad } from 'react-icons/md'
 
-export default function AddNode(){
+export default function AddNode() {
   return (
     <div className={styles.addButtonContainer}>
       <div className={styles.addButton}>
         <MdAdd className={styles.icon} />
       </div>
       <div className={styles.actionContainer}>
-        <div className={styles.actionButton}>
-          <div className={styles.actionIconContainer}>
-            <MdDialpad className={styles.actionIcon}/>
-          </div>
-          <p className={styles.actionLabel}>Menu</p>
+        <div className={styles.actionIconContainer}>
+          <MdDialpad className={styles.actionIcon} />
         </div>
+        <p className={styles.actionLabel}>Menu</p>
       </div>
     </div>
   )
