9f5383156afa3a2ea668c85a18d2d3ef920312dc
Caio Calisto <caiocesarcalisto@gmail.com>
2024-01-28 19:21:51 +0000

    responsive form

--- NEXTJS/responsive-app/src/app/page.tsx
+++ NEXTJS/responsive-app/src/app/page.tsx
@@ -11,6 +11,16 @@ export default function Home() {
       <br />
       <br />
       <p className="text-vw">This text uses vw</p>
+      <br />
+      <br />
+      <br />
+      <div className="my-form">
+        <p className="text-vw">Form</p>
+        <div className="my-form-line">
+          <input type="text" name="text1" id="text1" className="textbox" placeholder="Name" />
+          <input type="text" name="text1" id="text2" className="textbox" placeholder="Age" />
+        </div>
+      </div>
     </main>
   );
 }
