7bb58024c909350447d8d1b83e08100d7825e8f9
Caio Calisto <caiocesarcalisto@gmail.com>
2024-01-28 15:09:33 +0000

    responsive

---
+++ NEXTJS/responsive-app/src/app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <main className="">
+      <p className="text-rem">This text uses rem as unit</p>
+      <br />
+      <br />
+      <br />
+      <p className="text-percentage">This text uses percentage</p>
+      <br />
+      <br />
+      <br />
+      <p className="text-vw">This text uses vw</p>
+    </main>
+  );
+}
