65b710b4f59ce453737121fd6014705e33f5e6d9
Caio Calisto <caiocesarcalisto@gmail.com>
2024-02-10 13:00:56 +0000

    typography | item detail page

--- NEXTJS/second-app/src/app/commodity/[code]/page.tsx
+++ NEXTJS/second-app/src/app/commodity/[code]/page.tsx
@@ -1,8 +1,19 @@
+import { Typography } from '@/app/styled-components/typography/typography'
 import React from 'react'
 
 function CommodityView() {
   return (
-    <div>CommodityView</div>
+    <div className='flex pl-2 sm:pl-4 md:pl-8 lg:pl-12 xl:pl-20 pt-4'>
+      
+      {/* TITLE AND DESCRIPTION */}
+      <div className='flex flex-col'>
+        <Typography type='TITLE' size='S' weight='SEMIBOLD'>LPDE const fob US</Typography>
+        <Typography type='BODY' size='M' weight='REGULAR'>Low density polyethylene (LPDE) | large volume transactions | contract price</Typography>
+      </div>
+      {/* END TITLE AND DESCRIPTION */}
+
+      
+    </div>
   )
 }
 
