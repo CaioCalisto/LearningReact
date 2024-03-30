f2dcaba632720e080697349c9ec62fb1d801236e
Caio Calisto <caiocesarcalisto@gmail.com>
2024-03-30 14:35:39 +0000

    layout with sidebar

--- NEXTJS/commodities-app/src/app/layout.tsx
+++ NEXTJS/commodities-app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +12,27 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className}`}>
+        <div className={"h-screen flex overflow-hidden bg-gray-200"}>
+          <div
+            className={"flex-col bg-gray-800 w-24 py-6 px-2 hidden md:block"}
+          >
+            <Image
+              src={"/acme.svg"}
+              alt={"Logo"}
+              width={"0"}
+              height={"0"}
+              style={{ width: '100%', height: 'auto' }}
+            />
+          </div>
+        </div>
+        <main className={"flex-1 overflow-auto focus:outline-none"}>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- NEXTJS/commodities-app/tailwind.config.ts
+++ NEXTJS/commodities-app/tailwind.config.ts
@@ -1,16 +1,16 @@
-import type {Config} from "tailwindcss";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {},
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {},
     },
-    plugins: [],
+  },
+  plugins: [],
 };
 export default config;
