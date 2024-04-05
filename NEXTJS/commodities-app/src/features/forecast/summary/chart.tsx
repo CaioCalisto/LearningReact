54b5216fcf80f510cb4d7eff17ae1777b9b64531
Caio Calisto <caiocesarcalisto@gmail.com>
2024-04-05 23:27:22 +0100

    responsive is broken :(

--- NEXTJS/commodities-app/src/features/forecast/summary/chart.tsx
+++ NEXTJS/commodities-app/src/features/forecast/summary/chart.tsx
@@ -12,18 +12,15 @@ export default function Chart({ commodityId }: Props) {
   const { options } = useChart(commodityId);
 
   return (
-    <div className={"flex flex-row justify-between"}>
-      <div className={"flex flex-col gap-3"}>
-        <HighchartsReact highcharts={Highcharts} options={options} />
-        <div className={"flex flex-row gap-3"}>
-          <FilterButton>6M</FilterButton>
-          <FilterButton>1Y</FilterButton>
-          <FilterButton>3Y</FilterButton>
-          <FilterButton>5Y</FilterButton>
-          <FilterButton>All</FilterButton>
-        </div>
+    <div className={"flex flex-col gap-3"}>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+      <div className={"flex flex-row gap-3"}>
+        <FilterButton>6M</FilterButton>
+        <FilterButton>1Y</FilterButton>
+        <FilterButton>3Y</FilterButton>
+        <FilterButton>5Y</FilterButton>
+        <FilterButton>All</FilterButton>
       </div>
-      <RightSidePanel />
     </div>
   );
 }
