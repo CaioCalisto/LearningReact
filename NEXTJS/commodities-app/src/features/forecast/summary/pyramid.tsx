2480377e9da1fa8ebebe6f180c94691288f4d80e
Caio Calisto <caiocesarcalisto@gmail.com>
2024-04-07 22:58:15 +0100

    responsive svg

--- NEXTJS/commodities-app/src/features/forecast/summary/pyramid.tsx
+++ NEXTJS/commodities-app/src/features/forecast/summary/pyramid.tsx
@@ -68,8 +68,7 @@ export default function Pyramid() {
         key="pyramid"
         className="overflow-visible"
         xmlns="http://www.w3.org/2000/svg"
-        height="100%"
-        width="100%"
+        viewBox="0 0 600 470"
       >
         {getPyramidElements().map((element, index) => (
           <path
