6dd03452c8e94aadd376b06a7a416044e72dbbef
Caio Calisto <caiocesarcalisto@gmail.com>
2024-05-25 20:42:15 +0100

    refactoring highcharts mapper

---
+++ NEXTJS/commodities-app/src/features/charts/highchart-transform/CustomOptions.ts
@@ -0,0 +1,74 @@
+import { Options } from "highcharts";
+
+export class CustomOptions {
+  private highchartOptions: Options;
+
+  constructor() {
+    this.highchartOptions = {};
+  }
+
+  withTitle(title: string): this {
+    this.highchartOptions = {
+      ...this.highchartOptions,
+      title: { text: title },
+    };
+
+    return this;
+  }
+
+  withTooltip(message: string): this {
+    this.highchartOptions = {
+      ...this.highchartOptions,
+      tooltip: {
+        backgroundColor: "var(--GRAY-70)",
+        style: { color: "var(--GRAY-2)" },
+        formatter: function () {
+          return message;
+        },
+      },
+    };
+
+    return this;
+  }
+
+  withYAsisTitle(title: string): this {
+    this.highchartOptions = {
+      ...this.highchartOptions,
+      yAxis: { title: { text: title } },
+    };
+
+    return this;
+  }
+
+  withLineSeries(name: string, data: number[]): this {
+    this.highchartOptions = {
+      ...this.highchartOptions,
+      series: [
+        {
+          name: name,
+          type: "line",
+          data,
+          color: "var(--BLUE-50)",
+        },
+      ],
+    };
+
+    return this;
+  }
+
+  withXAsis(title: string, categories: string[]): this {
+    this.highchartOptions = {
+      ...this.highchartOptions,
+      xAxis: {
+        title: { text: title },
+        categories,
+      },
+    };
+
+    return this;
+  }
+
+  getOptions(): Options {
+    return this.highchartOptions;
+  }
+}
--- NEXTJS/commodities-app/src/features/forecast/summary/useChart.ts
+++ NEXTJS/commodities-app/src/features/forecast/summary/useChart.ts
@@ -1,43 +1,27 @@
 import { HighchartsReactProps } from "highcharts-react-official";
-import { Options } from "highcharts";
+import { CustomOptions } from "@/features/charts/highchart-transform/CustomOptions";
 
 export default function useChart(commodityId: string): HighchartsReactProps {
-  const options: Options = {
-    title: { text: "" },
-    series: [
-      {
-        name: commodityId,
-        type: "line",
-        data: [1, 2, 4, 3, 4, 1, 5, 2, 4, 3, 6, 5],
-        color: "var(--BLUE-50)",
-      },
-    ],
-    xAxis: {
-      title: { text: "Period" },
-      categories: [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-      ],
-    },
-    yAxis: { title: { text: "Value" } },
-    tooltip: {
-      backgroundColor: "var(--GRAY-70)",
-      style: { color: "var(--GRAY-2)" },
-      formatter: function () {
-        return `Custom message`;
-      },
-    },
-  };
+  const options = new CustomOptions()
+    .withTitle("")
+    .withLineSeries(commodityId, [1, 2, 4, 3, 4, 1, 5, 2, 4, 3, 6, 5])
+    .withXAsis("Period", [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ])
+    .withYAsisTitle("Value")
+    .withTooltip("Custom message")
+    .getOptions();
 
   return { options };
 }
